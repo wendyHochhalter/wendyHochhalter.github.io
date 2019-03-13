912058ed1d025ee751cf1fe768d8da65af95e1d7
Wen Z <hoc18003@byui.edu>
2019-03-13 19:41:59 -0400

    Discussion Board - Usability

---
+++ assignments/lesson-10/scripts/map.js
@@ -0,0 +1,6 @@
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoid2VuaG9jaCIsImEiOiJjanM4ZDEyMDAwZTQ1NDVtamhqczN0MXFiIn0.gxYX2jUKfX8x_A3UwTDZpw';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11'
+});
